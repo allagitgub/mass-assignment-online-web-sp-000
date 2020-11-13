d438ca32557f1ed5d8ffeef788f9d889d3276801
Alla Kuzminova <alchik@gmail.com>
2020-11-13 19:57:25 +0000

    Automatically backed up by Learn

---
+++ lib/custom_errors.rb
@@ -0,0 +1,26 @@
+require_relative './partner_error'
+
+class Person
+  attr_accessor :name, :partner
+
+  def initialize(name)
+    @name = name
+  end
+
+  def get_married(person)
+      self.partner = person
+      if person.class != Person
+        begin
+          raise PartnerError
+        rescue PartnerError => error
+          puts error.message
+        end
+      else
+        person.partner = self
+      end
+    end
+end
+
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
